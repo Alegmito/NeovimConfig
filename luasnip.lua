5ff1f949d85110143cf9a985df504d91e0a7af1d
Daniil Smirnov <daniil.smirnov007@gmail.com>
2024-08-12 21:22:03 +0300

    new configs

---
+++ luasnip.lua
@@ -0,0 +1,27 @@
+~\AppData\Local\nvim/..
+  .git
+  ★ lua
+    ★ alegmito
+      ★ plugins
+         cloak.lua
+         colors.lua
+         gdscript-extended-lsp.lua
+         harpoon.lua
+         init.lua
+         lsp.lua
+         lualine.lua
+         luarocks.lua
+         neotest.lua
+         neotree.lua
+         nvim-nio.lua
+         ★ nvim-tree.lua
+         telescope.lua
+         treesitter.lua
+         trouble.lua
+         undotree.lua
+       init.lua
+       lazy.lua
+       remap.lua
+       set.lua
+   init.lua
+   ✗ lazy-lock.json
